69b6442058840e0a019aa9d9b79729951fa2dab9
Marko Kosunen <marko.kosunen@aalto.fi>
2019-06-23 07:22:10 -0700

    Remove chisel form init submodules gor now

--- init_submodules.sh
+++ init_submodules.sh
@@ -3,15 +3,15 @@ DIR="$( cd "$( dirname $0 )" && pwd )"
 cd $DIR
 
 git submodule sync 
-for module in \
-    ./chisel; do
-    git submodule update --init $module
-    cd $module
-    if [ -f ./init_submodules.sh ]; then
-        ./init_submodules.sh
-        cd ${DIR}
-    fi
-done
+#for module in \
+#    ./chisel; do
+#    git submodule update --init $module
+#    cd $module
+#    if [ -f ./init_submodules.sh ]; then
+#        ./init_submodules.sh
+#        cd ${DIR}
+#    fi
+#done
 
 exit 0
 
